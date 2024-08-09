3c1a8093492441ac1e11d76b689f09a2984a3122
HazarathKumarM <hazarathkumar@multicorewareinc.com>
2024-08-09 14:23:27 +0000

    fix bug in the qa tests for pixelate

--- include/rppdefs.h
+++ include/rppdefs.h
@@ -1053,7 +1053,7 @@ typedef struct
     Rpp64u* dstBatchIndex;
     Rpp32u* inc;
     Rpp32u* dstInc;
-    hipMemRpp32u scratchBuf;
+    hipMemRpp32f scratchBufferPinned;
 } memGPU;
 
 /*! \brief RPP HIP-HOST memory management
