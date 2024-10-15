aa300b0ff82fc8d33e62a4733e01602ac3037eb3
dineshbabu-ravichandran <dineshbabu.ravichandran@multicorewareinc.com>
2024-10-15 09:29:16 +0000

    Doxygen Image changed

--- include/rppt_tensor_effects_augmentations.h
+++ include/rppt_tensor_effects_augmentations.h
@@ -689,8 +689,8 @@ RppStatus rppt_pixelate_gpu(RppPtr_t srcPtr, RpptDescPtr srcDescPtr, RppPtr_t ds
  * \details The Snow augmentation does a  modification of brightness on a batch of RGB(3 channel) / greyscale(1 channel) images with an NHWC/NCHW tensor layout.<br>
  * - srcPtr depth ranges - Rpp8u (0 to 255), Rpp16f (0 to 1), Rpp32f (0 to 1), Rpp8s (-128 to 127).
  * - dstPtr depth ranges - Will be same depth as srcPtr.
- * \image html img640x480.png Sample Input
- * \image html effects_augmentations_snow_img640x480.png Sample Output
+ * \image html img640x480_1.png Sample Input
+ * \image html effects_augmentations_snow_img640x480_1.png Sample Output
  * \param [in] srcPtr source tensor in HOST memory
  * \param [in] srcDescPtr source tensor descriptor (Restrictions - numDims = 4, offsetInBytes >= 0, dataType = U8/F16/F32/I8, layout = NCHW/NHWC, c = 1/3)
  * \param [out] dstPtr destination tensor in HOST memory
@@ -712,8 +712,8 @@ RppStatus rppt_snow_host(RppPtr_t srcPtr, RpptDescPtr srcDescPtr, RppPtr_t dstPt
  * \details The Snow augmentation does a modification of brightness on a batch of RGB(3 channel) / greyscale(1 channel) images with an NHWC/NCHW tensor layout.<br>
  * - srcPtr depth ranges - Rpp8u (0 to 255), Rpp16f (0 to 1), Rpp32f (0 to 1), Rpp8s (-128 to 127).
  * - dstPtr depth ranges - Will be same depth as srcPtr.
- * \image html img640x480.png Sample Input
- * \image html effects_augmentations_snow_img640x480.png Sample Output
+ * \image html img640x480_1.png Sample Input
+ * \image html effects_augmentations_snow_img640x480_1.png Sample Output
  * \param [in] srcPtr source tensor in HIP memory
  * \param [in] srcDescPtr source tensor descriptor (Restrictions - numDims = 4, offsetInBytes >= 0, dataType = U8/F16/F32/I8, layout = NCHW/NHWC, c = 1/3)
  * \param [out] dstPtr destination tensor in HIP memory
