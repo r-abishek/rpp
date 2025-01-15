d6fca2794d2258302f4c621e3b415713ae700c7d
Srihari-mcw <srihari@multicorewareinc.com>
2025-01-15 11:33:26 +0530

    Remove unused hpp files

--- src/modules/hip/kernel/copy.hpp
+++
@@ -1,9 +0,0 @@
-#include <hip/hip_runtime.h>
-#include "rpp_hip_common.hpp"
-
-template <typename T>
-RppStatus hip_exec_copy_tensor(T *srcPtr,
-                               RpptDescPtr srcDescPtr,
-                               T *dstPtr,
-                               RpptDescPtr dstDescPtr,
-                               rpp::Handle& handle);
