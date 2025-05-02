41bfd0c2d3a99aff7ca843f8cc4fcc936cef95ca
Srihari-mcw <srihari@multicorewareinc.com>
2025-05-02 16:52:26 +0530

    Add rppi_legacy_augmentations.h

---
+++ api/rppi_legacy_augmentations.h
@@ -0,0 +1,55 @@
+#include "rppdefs.h"
+#include "rpp.h"
+#ifdef __cplusplus
+extern "C"
+{
+#endif // cpusplus
+
+
+/******************** fisheye ********************/
+
+// Performs a fisheye transformation for a batch of images
+
+// *param[in] srcPtr Input image batch
+// *param[in] srcSize Array containing an RppiSize for each image in the batch
+// *param[in] maxSrcSize A single RppiSize which is the maxWidth and maxHeight for all images in the batch
+// *param[out] dstPtr Output image batch
+// *param[in] nbatchSize Batch size or the number of images in the batch
+// *param[in] rppHandle OpenCL-handle/HIP-handle for "_gpu" variants and Host-handle for "_host" variants
+// *returns a  RppStatus enumeration.
+// *retval RPP_SUCCESS : No error, Succesful completion
+// *retval RPP_ERROR : Error
+
+RppStatus rppi_fisheye_u8_pln1_batchPD_host(RppPtr_t srcPtr, RppiSize *srcSize, RppiSize maxSrcSize, RppPtr_t dstPtr, Rpp32u nbatchSize, rppHandle_t rppHandle);
+RppStatus rppi_fisheye_u8_pkd3_batchPD_host(RppPtr_t srcPtr, RppiSize *srcSize, RppiSize maxSrcSize, RppPtr_t dstPtr, Rpp32u nbatchSize, rppHandle_t rppHandle);
+#ifdef GPU_SUPPORT
+RppStatus rppi_fisheye_u8_pln1_batchPD_gpu(RppPtr_t srcPtr, RppiSize *srcSize, RppiSize maxSrcSize, RppPtr_t dstPtr, Rpp32u nbatchSize, rppHandle_t rppHandle);
+RppStatus rppi_fisheye_u8_pkd3_batchPD_gpu(RppPtr_t srcPtr, RppiSize *srcSize, RppiSize maxSrcSize, RppPtr_t dstPtr, Rpp32u nbatchSize, rppHandle_t rppHandle);
+#endif // GPU_SUPPORT
+
+/******************** snow ********************/
+
+// Adds a snowfall overlay for a batch of images
+
+// *param[in] srcPtr Input image batch
+// *param[in] srcSize Array containing an RppiSize for each image in the batch
+// *param[in] maxSrcSize A single RppiSize which is the maxWidth and maxHeight for all images in the batch
+// *param[out] dstPtr Output image batch
+// *param[in] snowValue Array containing an Rpp32f snow-value for each image in the batch (0 <= snowValue <= 1)
+// *param[in] nbatchSize Batch size or the number of images in the batch
+// *param[in] rppHandle OpenCL-handle/HIP-handle for "_gpu" variants and Host-handle for "_host" variants
+// *returns a  RppStatus enumeration.
+// *retval RPP_SUCCESS : No error, Succesful completion
+// *retval RPP_ERROR : Error
+
+RppStatus rppi_snow_u8_pln1_batchPD_host(RppPtr_t srcPtr, RppiSize *srcSize, RppiSize maxSrcSize, RppPtr_t dstPtr, Rpp32f *snowValue, Rpp32u nbatchSize, rppHandle_t rppHandle);
+RppStatus rppi_snow_u8_pkd3_batchPD_host(RppPtr_t srcPtr, RppiSize *srcSize, RppiSize maxSrcSize, RppPtr_t dstPtr, Rpp32f *snowValue, Rpp32u nbatchSize, rppHandle_t rppHandle);
+#ifdef GPU_SUPPORT
+RppStatus rppi_snow_u8_pln1_batchPD_gpu(RppPtr_t srcPtr, RppiSize *srcSize, RppiSize maxSrcSize, RppPtr_t dstPtr, Rpp32f *snowValue, Rpp32u nbatchSize, rppHandle_t rppHandle);
+RppStatus rppi_snow_u8_pkd3_batchPD_gpu(RppPtr_t srcPtr, RppiSize *srcSize, RppiSize maxSrcSize, RppPtr_t dstPtr, Rpp32f *snowValue, Rpp32u nbatchSize, rppHandle_t rppHandle);
+#endif // GPU_SUPPORT
+
+#ifdef __cplusplus
+extern "C"
+}
+#endif // cpusplus
