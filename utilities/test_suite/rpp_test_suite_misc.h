7e9975aabfa1989cdee07f7b9053b448c837607d
Srihari-mcw <srihari@multicorewareinc.com>
2025-10-09 06:04:46 +0530

    Update cutoff checks

--- utilities/test_suite/rpp_test_suite_misc.h
+++ utilities/test_suite/rpp_test_suite_misc.h
@@ -590,7 +590,7 @@ void compare_output(void *output, Rpp32u nDim, Rpp32u batchSize, Rpp32u bitDepth
 
         if(testCase == "log" && bitDepth == 4)
         {
-            Rpp32f cutoff = 1e-20;
+            Rpp32f cutoff = 1e-6;
 
             Rpp32f *ref = static_cast<Rpp32f *>(refOutput) + sampleOffset;
             Rpp32f *out = static_cast<Rpp32f *>(output) + i * sampleLength;
@@ -602,7 +602,7 @@ void compare_output(void *output, Rpp32u nDim, Rpp32u batchSize, Rpp32u bitDepth
         }
         else if(bitDepth == 2 || bitDepth == 11 || bitDepth == 4)  // F32 || I16_F32 || U8_F32
         {
-            Rpp32f cutoff = (testCase == "normalize") ? 1e-5 : 1e-20;
+            Rpp32f cutoff = (testCase == "normalize") ? 1e-5 : 1e-6;
 
             Rpp32f *ref = static_cast<Rpp32f *>(refOutput) + sampleOffset;
             Rpp32f *out = static_cast<Rpp32f *>(output) + i * sampleLength;
