a75f687c0d32bc93c2a8a0fd89385bdab82dc180
Srihari-mcw <srihari@multicorewareinc.com>
2025-10-23 12:24:36 +0530

    Modify function calls to use rppt_brightness

--- utilities/test_suite/HIP/Tensor_image_hip.cpp
+++ utilities/test_suite/HIP/Tensor_image_hip.cpp
@@ -689,7 +689,7 @@ int main(int argc, char **argv)
 
                     startWallTime = omp_get_wtime();
                     if (BitDepthTestMode == U8_TO_U8 || BitDepthTestMode == F16_TO_F16 || BitDepthTestMode == F32_TO_F32 || BitDepthTestMode == I8_TO_I8)
-                        rppt_brightness_gpu(d_input, srcDescPtr, d_output, dstDescPtr, alpha, beta, roiTensorPtrSrc, roiTypeSrc, handle);
+                        rppt_brightness(d_input, srcDescPtr, d_output, dstDescPtr, alpha, beta, roiTensorPtrSrc, roiTypeSrc, handle);
                     else
                         missingFuncFlag = 1;
 
