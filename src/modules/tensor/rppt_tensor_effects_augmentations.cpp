a6da6650189223cd2bdeddc85a2be63e2bd5219b
Srihari-mcw <srihari@multicorewareinc.com>
2025-11-26 18:21:49 +0530

    Update all reinterpret cast

--- src/modules/tensor/rppt_tensor_effects_augmentations.cpp
+++ src/modules/tensor/rppt_tensor_effects_augmentations.cpp
@@ -2007,9 +2007,9 @@ RppStatus rppt_jitter(RppPtr_t srcPtr,
         }
         else if ((srcDescPtr->dataType == RpptDataType::F32) && (dstDescPtr->dataType == RpptDataType::F32))
         {
-            hip_exec_jitter_tensor((Rpp32f*) (static_cast<Rpp8u*>(srcPtr) + srcDescPtr->offsetInBytes),
+            hip_exec_jitter_tensor(reinterpret_cast<Rpp32f*>(static_cast<Rpp8u*>(srcPtr) + srcDescPtr->offsetInBytes),
                                    srcDescPtr,
-                                   (Rpp32f*) (static_cast<Rpp8u*>(dstPtr) + dstDescPtr->offsetInBytes),
+                                   reinterpret_cast<Rpp32f*>(static_cast<Rpp8u*>(dstPtr) + dstDescPtr->offsetInBytes),
                                    dstDescPtr,
                                    kernelSizeTensor,
                                    d_xorwowInitialStatePtr,
