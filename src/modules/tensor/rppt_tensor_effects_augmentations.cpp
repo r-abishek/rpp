37fcb9faaeb4979fb0f18f6e7a45d14706092471
Srihari-mcw <srihari@multicorewareinc.com>
2025-11-27 08:38:30 +0530

    Update with reinterpret cast

--- src/modules/tensor/rppt_tensor_effects_augmentations.cpp
+++ src/modules/tensor/rppt_tensor_effects_augmentations.cpp
@@ -1997,7 +1997,7 @@ RppStatus rppt_jitter(RppPtr_t srcPtr,
         {
             hip_exec_jitter_tensor(reinterpret_cast<half*>(static_cast<Rpp8u*>(srcPtr) + srcDescPtr->offsetInBytes),
                                    srcDescPtr,
-                                   (half*) (static_cast<Rpp8u*>(dstPtr) + dstDescPtr->offsetInBytes),
+                                   reinterpret_cast<half*>(static_cast<Rpp8u*>(dstPtr) + dstDescPtr->offsetInBytes),
                                    dstDescPtr,
                                    kernelSizeTensor,
                                    d_xorwowInitialStatePtr,
